e38131ab0ef6e7407cd1e30fb6876dfe3778e671
Marc Foley <m.foley.88@gmail.com>
2017-04-28 12:06:01 +0100

    refactored get_familyname

--- fontdata.py
+++ fontdata.py
@@ -1,30 +1,30 @@
 from nototools import font_data
-from fontTools.ttLib import TTFont
 
-WIN_NAME_SUFFIXES = [
-    " Thin",
-    " ExtraLight",
-    " Extralight",
-    " SemiLight",
-    " Light",
-    " Regular",
-    " Medium",
-    " SemiBold",
-    " Semibold",
-    " Bold",
-    " ExtraBold",
-    " Extrabold",
-    " Black",
-    " VF Beta",
+
+STYLES = [
+    "Thin",
+    "ExtraLight",
+    "Extralight",
+    "SemiLight",
+    "Light",
+    "Regular",
+    "Medium",
+    "SemiBold",
+    "Semibold",
+    "Bold",
+    "ExtraBold",
+    "Extrabold",
+    "Black",
+    "VF Beta",
 ]
 
 def get_familyname(ttfont):
     """Get the name of a font file"""
     name = ttfont['name'].getName(1, 3, 1, 1033).string
     name = name.decode('utf_16_be')
-    for suffix in WIN_NAME_SUFFIXES:
-        if suffix in name:
-            name = name.replace(suffix, '')
+    for style in STYLES:
+        if style in name:
+            name = name.replace(' '+style, '')
     return name
 
 
