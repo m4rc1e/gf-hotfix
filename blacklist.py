53aff574b4045c6c4823eb1ed14dc350ea0f41d3
Marc Foley <m.foley.88@gmail.com>
2017-04-29 20:33:33 +0100

    added blacklisted fonts

--- blacklist.py
+++ blacklist.py
@@ -1,9 +1,5 @@
 fonts = [
     'Andika',  # fake canonical
-    'Abril Fatface', # non ascii
-    'Carme', # None ascii
     'Lancelot', # Name imbalance
-    'Convergence',
-    'Duru Sans',
-    'Exo SemiBold Italic'
+    'Khand',  # Very bad nametable 2 entries only
 ]
