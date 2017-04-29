56f69e4e1feca5a08001115f6d03426176b6e986
Marc Foley <m.foley.88@gmail.com>
2017-04-29 18:32:51 +0100

    ignore these fonts

---
+++ blacklist.py
@@ -0,0 +1,9 @@
+fonts = [
+    'Andika',  # fake canonical
+    'Abril Fatface', # non ascii
+    'Carme', # None ascii
+    'Lancelot', # Name imbalance
+    'Convergence',
+    'Duru Sans',
+    'Exo SemiBold Italic'
+]
