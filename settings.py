4652a052c515dc3be9a6834bf7b2c9365976cc8b
Marc Foley <m.foley.88@gmail.com>
2017-04-30 14:24:47 +0100

    added pouction_fonts_renamed_dir

--- settings.py
+++ settings.py
@@ -1,8 +1,14 @@
 import os
 
 production_fonts_dir = os.path.join(
-        os.path.dirname(__file__),
-        'bin',
-        'production_fonts'
-    )
+    os.path.dirname(__file__),
+    'bin',
+    'production_fonts'
+)
+
+production_fonts_renamed_dir = os.path.join(
+    os.path.dirname(__file__),
+    'bin',
+    'production_fonts_renamed'
+)
 gf_api_url = 'http://tinyurl.com/m8o9k39'
