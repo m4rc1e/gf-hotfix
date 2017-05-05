133661f2613e8f020fb499c85b879f6f78b23f2b
Marc Foley <m.foley.88@gmail.com>
2017-05-05 12:23:25 +0100

    add reports folder if it does not exist

--- generate_reports_before_fix.py
+++ generate_reports_before_fix.py
@@ -1,6 +1,10 @@
+import os
 from settings import (
     production_fonts_renamed_dir
 )
 from report import check_fonts
+
+if not os.path.isdir('reports'):
+    os.mkdir('reports')
 
 check_fonts(production_fonts_renamed_dir, 'current')
