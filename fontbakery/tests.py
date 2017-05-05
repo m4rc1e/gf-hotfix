f714e52aea2e79a4bc03c0105fcc90a4bd72fc17
Marc Foley <m.foley.88@gmail.com>
2017-05-05 15:47:55 +0100

    added gfspec tests

--- fontbakery/tests.py
+++ fontbakery/tests.py
@@ -22,6 +22,20 @@ class TestGFSpec(unittest.TestCase):
     def test_get_fsselection(self):
         pass
 
+    def test_macstyle(self):
+        self.assertEqual(gfspec.get_macstyle('Family-Regular.ttf'), 0)
+        self.assertEqual(gfspec.get_macstyle('Family-Italic.ttf'), 2)
+        self.assertEqual(gfspec.get_macstyle('Family-Bold.ttf'), 1)
+        self.assertEqual(gfspec.get_macstyle('Family-BoldItalic.ttf'), 3)
+
+    def test_weightclass(self):
+        self.assertEqual(gfspec.get_weightclass('Family-Thin.ttf'), 250)
+        self.assertEqual(gfspec.get_weightclass('Family-ThinItalic.ttf'), 250)
+
+        self.assertEqual(gfspec.get_weightclass('Family-Regular.ttf'), 400)
+        self.assertEqual(gfspec.get_weightclass('Family-Italic.ttf'), 400)
+        
+
 
 if __name__ == '__main__':
     unittest.main()
